692820e00262f51b736e9bc3b58a4b339d0436f2
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-26 21:32:36 +0000

    Standard page metadata

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,52 @@
-'use client';
-
 // Imports
 // ------------
 import React from 'react';
-import DeleteMe from '@parts/DeleteMe';
 
 // Component
 // ------------
 const Page = () => {
 	return (
 		<>
-			<DeleteMe />
+			{/*  */}
+			{/*  */}
+			{/*  */}
 		</>
 	);
 };
 
+// SEO Metadata
+// ------------
+export const metadata = {
+	title: 'please_change_this',
+	description: 'please_change_this',
+	openGraph: {
+		ogTitle: 'please_change_this',
+		ogDescription: 'please_change_this',
+		ogUrl: 'please_change_this',
+		ogImages: [
+			{
+				url: 'please_change_this',
+				width: 1200,
+				height: 630,
+				alt: 'please_change_this',
+			},
+		],
+	},
+	twitter: {
+		twitterCard: 'please_change_this',
+		twitterTitle: 'please_change_this',
+		twitterDescription: 'please_change_this',
+		twitterImage: 'please_change_this',
+	},
+	schema: {
+		schemaType: 'please_change_this',
+		schemaName: 'please_change_this',
+		schemaDescription: 'please_change_this',
+		schemaUrl: 'please_change_this',
+		schemaImage: 'please_change_this',
+	},
+};
+
+// Exports
+// ------------
 export default Page;

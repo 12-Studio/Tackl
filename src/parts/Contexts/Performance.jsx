946ba9ec695af904055734584415320e635eead6
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-28 20:48:44 +0000

    Fix for performance file

--- src/parts/Contexts/Performance.jsx
+++ src/parts/Contexts/Performance.jsx
@@ -11,29 +11,24 @@ export const PerformanceContext = createContext({});
 // Component
 // ------------
 export const PerformanceProvider = ({ children }) => {
-	const value = useMemo(
-		() => ({
-			// Checks if user has requested reduced motion
-			// Useful for disabling/reducing animations for accessibility
-			isReducedMotion: window?.matchMedia('(prefers-reduced-motion: reduce)')
-				.matches,
+    const value = useMemo(
+        () => ({
+            // Checks if user has requested reduced motion
+            // Useful for disabling/reducing animations for accessibility
+            isReducedMotion: window?.matchMedia('(prefers-reduced-motion: reduce)').matches,
 
-			// Detects if device is in low-power mode
-			// Can be used to disable heavy animations or effects
-			isLowPowerMode: navigator?.userAgent.includes('Low-Power'),
+            // Detects if device is in low-power mode
+            // Can be used to disable heavy animations or effects
+            isLowPowerMode: navigator?.userAgent.includes('Low-Power'),
 
-			// Gets screen pixel density
-			// Useful for serving appropriate image resolutions
-			devicePixelRatio: window?.devicePixelRatio || 1,
-		}),
-		[]
-	);
+            // Gets screen pixel density
+            // Useful for serving appropriate image resolutions
+            devicePixelRatio: typeof window !== 'undefined' ? window.devicePixelRatio : 1,
+        }),
+        []
+    );
 
-	return (
-		<PerformanceContext.Provider value={value}>
-			{children}
-		</PerformanceContext.Provider>
-	);
+    return <PerformanceContext.Provider value={value}>{children}</PerformanceContext.Provider>;
 };
 
 // For documentation, see `docs/PerformanceContext.md`

f49b6a388481e0bce81b468e24a2ca6cb6eaa099
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-28 10:39:24 +0000

    Update type.js
    Optimised for performance

--- src/theme/tackl/type.js
+++ src/theme/tackl/type.js
@@ -1,130 +1,110 @@
 // Imports
 // ------
 import { css } from 'styled-components';
-import { breakpointUp } from './breakpoints';
+import { breakpointUp as bp } from './breakpoints';
 import { semantics, gridSemantics } from './semantics';
 import { theme } from '../';
 
-// Exports
-// --------------
-// 1. Shared Heading Styles
-// --------------
-const sharedHeadingStyles = (props) => css`
-	font-weight: ${theme.font.weight.reg};
-	font-family: ${theme.font.type.heading};
+// Pre-compute commonly used values
+const {
+	font: {
+		weight: { reg, semi, medium },
+		type: { heading, body },
+	},
+} = theme;
+
+// Shared styles as constants to avoid recalculation
+const baseHeadingStyles = css`
+	font-weight: ${reg};
+	font-family: ${heading};
 	${gridSemantics}
 	${semantics}
 `;
 
-// --------------
-// 2. Heading Styles
-// --------------
+const baseBodyStyles = css`
+	font-family: ${body};
+	font-weight: ${reg};
+	${gridSemantics}
+	${semantics}
+`;
+
+// Heading styles with memoized shared styles
 export const h0Styles = css`
-	${sharedHeadingStyles}
+	${baseHeadingStyles}
 	font-size: 6rem;
 	line-height: 1.2;
-
-	${breakpointUp.smedium`
-		font-size: 6.8rem;
-	`}
-
-	${breakpointUp.xlarge`
-		font-size: 16.8rem;
-	`}
+	${bp.smedium`font-size: 6.8rem;`}
+	${bp.xlarge`font-size: 16.8rem;`}
 `;
 
 export const h1Styles = css`
-	${sharedHeadingStyles}
+	${baseHeadingStyles}
 	font-size: 5.8rem;
 	line-height: 1.2;
-
-	${breakpointUp.xlarge`
+	${bp.xlarge`
 		font-size: 14.4rem;
 		line-height: 1;
 	`}
 `;
 
 export const h2Styles = css`
-	${sharedHeadingStyles}
+	${baseHeadingStyles}
 	font-size: 4.8rem;
 	line-height: 1.2;
-
-	${breakpointUp.xlarge` font-size: 9rem; `}
+	${bp.xlarge`font-size: 9rem;`}
 `;
 
 export const h3Styles = css`
-	${sharedHeadingStyles}
+	${baseHeadingStyles}
 	font-size: 4rem;
 	line-height: 1.32;
-
-	${breakpointUp.xlarge` font-size: 6rem; `}
+	${bp.xlarge`font-size: 6rem;`}
 `;
 
 export const h4Styles = css`
-	${sharedHeadingStyles}
+	${baseHeadingStyles}
 	font-size: 3rem;
 	line-height: 1.32;
-
-	${breakpointUp.large` font-size: 4.8rem; `}
+	${bp.large`font-size: 4.8rem;`}
 `;
 
 export const h5Styles = css`
-	${sharedHeadingStyles}
+	${baseHeadingStyles}
 	font-size: 2.4rem;
 	line-height: 1.32;
-
-	${breakpointUp.large` font-size: 4rem; `}
+	${bp.large`font-size: 4rem;`}
 `;
 
-export const h6Styles = (props) => css`
-	${sharedHeadingStyles}
+export const h6Styles = css`
+	${baseHeadingStyles}
 	font-size: 1.2rem;
 	line-height: 1.32;
-	font-weight: ${theme.font.weight.semi};
+	font-weight: ${semi};
 	text-transform: uppercase;
-
-	${breakpointUp.large` font-size: 1.4rem; `}
+	${bp.large`font-size: 1.4rem;`}
 `;
 
-// --------------
-// 3. Shared Body Styles
-// --------------
-const sharedBodyStyles = (props) => css`
-	font-family: ${theme.font.type.body};
-	font-weight: ${theme.font.weight.reg};
-	${gridSemantics}
-	${semantics}
-`;
-
-// --------------
-// 3. Body Styles
-// --------------
-export const pStyles = (props) => css`
-	${sharedBodyStyles}
+// Body styles with memoized shared styles
+export const pStyles = css`
+	${baseBodyStyles}
 	font-size: 1.6rem;
 	line-height: 1.44;
-
-	${breakpointUp.xlarge`
-		font-size: 2.2rem;
-	`}
+	${bp.xlarge`font-size: 2.2rem;`}
 `;
 
-export const spanStyles = (props) => css`
-	${sharedBodyStyles}
-
+export const spanStyles = css`
+	${baseBodyStyles}
 	font-size: 1.4rem;
 	line-height: 1.32;
-
-	${breakpointUp.xlarge` font-size: 1.8rem; `}
+	${bp.xlarge`font-size: 1.8rem;`}
 `;
 
-export const emStyles = (props) => css`
-	${sharedBodyStyles}
+export const emStyles = css`
+	${baseBodyStyles}
 	display: inline-block;
 	font-style: normal;
-	font-weight: ${theme.font.weight.medium};
+	font-weight: ${medium};
 	font-size: 1rem;
 	line-height: 1.32;
-
-	${breakpointUp.xlarge` font-size: 1.2rem; `}
+	${bp.xlarge`font-size: 1.2rem;`}
 `;

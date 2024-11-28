3955cb98e8f506c8d13b28383ca1a42eeb9671b3
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-28 20:49:38 +0000

    Update for how colours are used

--- src/parts/DeleteMe/styles.js
+++ src/parts/DeleteMe/styles.js
@@ -6,10 +6,10 @@ import {} from '@tackl/type';
 
 // Exports
 // ------------
-const pulse = (props) => keyframes`
+const pulse = props => keyframes`
 	from {
 		transform: scale(1) translate(-50%, -50%);
-		border-color: ${props.theme.colors.global.white.solid};
+		border-color: ${props.theme.colors.global.white[100]};
 	}
 
 	to {
@@ -19,110 +19,110 @@ const pulse = (props) => keyframes`
 `;
 
 export const Jacket = styled(Section)(
-	(props) => css`
-		position: relative;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100svh;
-		background: ${props.theme.colors.brand.bc2};
-		overflow: hidden;
+    props => css`
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100svh;
+        background: ${props.theme.colors.brand.bc2[100]};
+        overflow: hidden;
 
-		picture {
-			position: relative;
-			z-index: 2;
-			display: flex;
-			align-items: center;
-			justify-content: center;
+        picture {
+            position: relative;
+            z-index: 2;
+            display: flex;
+            align-items: center;
+            justify-content: center;
 
-			width: 24rem;
-			height: 24rem;
-			background: ${props.theme.colors.global.white.solid};
-			border-radius: 50%;
+            width: 24rem;
+            height: 24rem;
+            background: ${props.theme.colors.global.white[100]};
+            border-radius: 50%;
 
-			&:before {
-				content: '';
-				position: absolute;
-				z-index: 1;
-				inset: 0.6rem;
-				background: ${props.theme.colors.brand.bc1};
-				border-radius: 50%;
-			}
+            &:before {
+                content: '';
+                position: absolute;
+                z-index: 1;
+                inset: 0.6rem;
+                background: ${props.theme.colors.brand.bc1[100]};
+                border-radius: 50%;
+            }
 
-			&:after {
-				content: '';
-				position: absolute;
-				z-index: 2;
-				inset: 1.2rem;
-				background: ${props.theme.colors.global.white.solid};
-				border-radius: 50%;
-			}
-		}
+            &:after {
+                content: '';
+                position: absolute;
+                z-index: 2;
+                inset: 1.2rem;
+                background: ${props.theme.colors.global.white[100]};
+                border-radius: 50%;
+            }
+        }
 
-		img {
-			position: relative;
-			z-index: 3;
-			width: 100%;
-			max-width: 12rem;
-			height: auto;
-		}
-	`
+        img {
+            position: relative;
+            z-index: 3;
+            width: 100%;
+            max-width: 12rem;
+            height: auto;
+        }
+    `
 );
 
 export const Pulse = styled.div(
-	(props) => css`
-		position: absolute;
-		z-index: -1;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
+    props => css`
+        position: absolute;
+        z-index: -1;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
 
-		z-index: 1;
-		width: 24rem;
-		height: 24rem;
-		border-radius: 100%;
+        z-index: 1;
+        width: 24rem;
+        height: 24rem;
+        border-radius: 100%;
 
-		span {
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			display: block;
-			width: 100%;
-			height: 100%;
+        span {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            display: block;
+            width: 100%;
+            height: 100%;
 
-			&:before,
-			&:after {
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-				transform-origin: top left;
-				content: '';
-				width: 90%;
-				height: 90%;
-				border: 1px solid ${props.theme.colors.global.white.solid};
-				border-radius: 100%;
-				animation: ${pulse} 3s ease-in-out infinite;
-			}
+            &:before,
+            &:after {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                transform-origin: top left;
+                content: '';
+                width: 90%;
+                height: 90%;
+                border: 1px solid ${props.theme.colors.global.white[100]};
+                border-radius: 100%;
+                animation: ${pulse} 3s ease-in-out infinite;
+            }
 
-			&:first-child {
-				&:before {
-					animation-delay: 1s;
-				}
-				&:after {
-					animation-delay: 1.5s;
-				}
-			}
+            &:first-child {
+                &:before {
+                    animation-delay: 1s;
+                }
+                &:after {
+                    animation-delay: 1.5s;
+                }
+            }
 
-			&:last-child {
-				&:before {
-					animation-delay: 0s;
-				}
-				&:after {
-					animation-delay: 0.5s;
-				}
-			}
-		}
-	`
+            &:last-child {
+                &:before {
+                    animation-delay: 0s;
+                }
+                &:after {
+                    animation-delay: 0.5s;
+                }
+            }
+        }
+    `
 );

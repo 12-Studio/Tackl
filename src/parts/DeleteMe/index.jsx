9b42f437057a3e8cc895c05072b07f65a7a5982a
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-28 12:16:41 +0000

    Update index.jsx

--- src/parts/DeleteMe/index.jsx
+++ src/parts/DeleteMe/index.jsx
@@ -9,42 +9,19 @@ import PropTypes from 'prop-types';
 // ------------
 import { Jacket, Pulse } from './styles';
 
-/**
- * DeleteMe Component
- *
- * A sample component that demonstrates proper React component structure.
- * Shows a logo with a pulsing animation effect.
- *
- * @component
- * @example
- * return (
- *   <DeleteMe
- *     logoSrc="./logo.svg"
- *     logoAlt="Company Logo"
- *     pulseCount={2}
- *   />
- * )
- */
-
 // Component
 // ------------
-const DeleteMe = ({
-	logoSrc = './logo.svg',
-	logoAlt = 'Tackl Logo',
-	pulseCount = 2,
-}) => {
-	// Create pulse elements based on count
-	const pulseElements = Array.from({ length: pulseCount }, (_, i) => (
-		<span key={i} />
-	));
-
+const DeleteMe = ({ logoSrc = './logo.svg', logoAlt = 'Tackl Logo' }) => {
 	return (
 		<Jacket>
 			<picture>
 				<img src={logoSrc} alt={logoAlt} />
 			</picture>
 
-			<Pulse>{pulseElements}</Pulse>
+			<Pulse>
+				<span />
+				<span />
+			</Pulse>
 		</Jacket>
 	);
 };
@@ -56,8 +33,6 @@ DeleteMe.propTypes = {
 	logoSrc: PropTypes.string,
 	/** Alt text for the logo image */
 	logoAlt: PropTypes.string,
-	/** Number of pulse elements to render */
-	pulseCount: PropTypes.number,
 };
 
 // Exports

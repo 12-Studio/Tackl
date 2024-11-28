da98dca96594994f2ff7a5649672faac58fe6f6e
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-28 10:41:20 +0000

    Update index.ts
    Optimised performance of the grid

--- src/theme/waffl/index.ts
+++ src/theme/waffl/index.ts
@@ -17,74 +17,55 @@ interface GridInterface {
 	$noGutter?: boolean;
 }
 
-// Exports
-// ------------
-/**
- * @name Waffl 3.0
- *
-	 * @param $noMargin - This will add padding to the sides of your grid
-	 * @param $isFixed - This will make your grid fixed width based on your theme settings (1440px)
-	 * @param $isFullscreen - This will make your grid fullscreen (100lvh)
-	 * @param $isFullscreenTop - This will make your grid fullscreen (100svh)
-	 * @param $isCenter - This will center your grid items vertically & horizontally
-	 * @param $noGutter - This will get rid of all gutters in your grid
- *
- * @important Children must be a semantic imported from tackl
- */
-
 // Constants
+// ------------
 const { columns, gutter, maxSize } = theme.grid;
+const { small, medium, large } = gutter;
 
-// Helper Functions
-const generateGridTemplate = (cols: number) => `repeat(${cols}, 1fr)`;
-const generateGutter = (noGutter: boolean, size: string) => (noGutter ? 0 : size);
+// Pre-computed grid templates for better performance
+const mobileGrid = `repeat(${columns.mobile}, 1fr)`;
+const mediumGrid = `repeat(${columns.medium}, 1fr)`;
+const largeGrid = `repeat(${columns.large}, 1fr)`;
+
+// Base styles to reduce recalculation
+const baseGridStyles = css`
+	display: grid;
+	margin: 0 auto;
+	width: 100%;
+`;
+
+// Helper function for gutter calculation
+const getGutter = (noGutter: boolean, size: string) => noGutter ? '0' : size;
 
 export const Grid = styled(Div)(
 	(props: GridInterface) => css`
-		display: grid;
-		grid-template-columns: ${generateGridTemplate(columns.mobile)};
-		column-gap: ${generateGutter(props.$noGutter, gutter.small)};
-
-		padding-left: ${theme.grid.gutter.small};
-		padding-right: ${theme.grid.gutter.small};
-		margin: 0 auto;
-		width: 100%;
-		max-width: ${props.$isFixed ? theme.grid.maxSize : 'none'};
+		${baseGridStyles}
+		grid-template-columns: ${mobileGrid};
+		column-gap: ${getGutter(props.$noGutter, small)};
+		padding-left: ${small};
+		padding-right: ${small};
+		max-width: ${props.$isFixed ? maxSize : 'none'};
 
 		${bp.medium`
-			grid-template-columns: ${generateGridTemplate(columns.medium)};
-			column-gap: ${generateGutter(props.$noGutter, gutter.medium)};
-			padding-left: ${theme.grid.gutter.medium};
-			padding-right: ${theme.grid.gutter.medium};
+			grid-template-columns: ${mediumGrid};
+			column-gap: ${getGutter(props.$noGutter, medium)};
+			padding-left: ${medium};
+			padding-right: ${medium};
 		`}
 
 		${bp.large`
-			grid-template-columns: ${generateGridTemplate(columns.large)};
-    		column-gap: ${generateGutter(props.$noGutter, gutter.large)};
-			padding-left: calc(${gutter.large} / 2);
-			padding-right: calc(${gutter.large} / 2);
+			grid-template-columns: ${largeGrid};
+			column-gap: ${getGutter(props.$noGutter, large)};
+			padding-left: calc(${large} / 2);
+			padding-right: calc(${large} / 2);
 		`}
 
-
-		/* NOTE • Side Padding for Parent Grids */
-		${props.$noMargin && css`
-			padding: 0;
-		`}
-
-		/* NOTE • Fullscreen */
-		${props.$isFullscreen && css`
-			height: 100lvh;
-		`}
-
-		${props.$isFullscreenTop && css`
-			height: 100svh;
-		`}
-
-		/* NOTE • Center Content */
+		${props.$noMargin && css`padding: 0;`}
+		${props.$isFullscreen && css`height: 100lvh;`}
+		${props.$isFullscreenTop && css`height: 100svh;`}
 		${props.$isCenter && css`
 			align-items: center;
 			justify-content: center;
 		`}
-
 	`
 );

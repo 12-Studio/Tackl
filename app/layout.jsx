a58324ae86e90c713b42cc1c959ff4e719ac77e3
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2024-11-28 22:19:39 +0000

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,7 @@ import { ApolloWrapper } from '@utils/apollo-wrapper';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@theme';
 import { Inter } from 'next/font/google';
+// import localFont from 'next/font/local';
 
 // Styles
 // ------------
@@ -42,6 +43,29 @@ const inter = Inter({
     variable: '--inter',
     preload: true,
 });
+// Local font configuration
+// const customFont = localFont({
+//     src: [
+//         {
+//             path: '../public/fonts/CustomFont-Regular.woff2',
+//             weight: '400',
+//             style: 'normal',
+//         },
+//         {
+//             path: '../public/fonts/CustomFont-Medium.woff2',
+//             weight: '500',
+//             style: 'normal',
+//         },
+//         {
+//             path: '../public/fonts/CustomFont-Bold.woff2',
+//             weight: '700',
+//             style: 'normal',
+//         }
+//     ],
+//     display: 'swap',
+//     variable: '--custom-font',
+//     preload: true,
+// });
 
 // Component
 // ------------

9488de00608437ae6989fbd7fc47202d96326635
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-05-13 10:12:42 +0100

    🐛 Added setContext import
    Type: bug
    Build: ⏩ Skipped

--- src/utils/apollo-wrapper.js
+++ src/utils/apollo-wrapper.js
@@ -2,11 +2,8 @@
 
 import React from 'react';
 import { ApolloLink, HttpLink, ApolloProvider } from '@apollo/client';
-import {
-	ApolloClient,
-	SSRMultipartLink,
-	InMemoryCache,
-} from '@apollo/experimental-nextjs-app-support';
+import { ApolloClient, SSRMultipartLink, InMemoryCache } from '@apollo/experimental-nextjs-app-support';
+// import { setContext } from '@apollo/client/link/context';
 
 // Configuration for Apollo Client
 const GRAPHQL_ENDPOINT = 'https://levinriegner.com/graphql';
@@ -21,17 +18,17 @@ const GRAPHQL_ENDPOINT = 'https://levinriegner.com/graphql';
  * - Easy auth token integration (see commented section)
  */
 function makeClient() {
-	// Basic HTTP connection to GraphQL endpoint
-	const httpLink = new HttpLink({
-		uri: GRAPHQL_ENDPOINT,
-		// Optional performance settings
-		fetchOptions: {
-			cache: 'no-store', // Disable fetch cache for fresh data
-		},
-	});
+    // Basic HTTP connection to GraphQL endpoint
+    const httpLink = new HttpLink({
+        uri: GRAPHQL_ENDPOINT,
+        // Optional performance settings
+        fetchOptions: {
+            cache: 'no-store', // Disable fetch cache for fresh data
+        },
+    });
 
-	// Uncomment and configure to add authentication
-	/* 
+    // Uncomment and configure to add authentication
+    /* 
 	const authLink = setContext((_, { headers }) => {
 		const token = 'YOUR_AUTH_TOKEN';
 		return {
@@ -43,40 +40,40 @@ function makeClient() {
 	});
 	*/
 
-	// Create optimized Apollo Client
-	return new ApolloClient({
-		// Configure cache
-		cache: new InMemoryCache({
-			typePolicies: {
-				Query: {
-					fields: {
-						// Add field policies here for cache behavior
-					},
-				},
-			},
-		}),
-		// Configure network layer based on environment
-		link:
-			typeof window === 'undefined'
-				? ApolloLink.from([
-						// Enable streaming responses in SSR
-						new SSRMultipartLink({
-							stripDefer: true,
-						}),
-						httpLink,
-				  ])
-				: httpLink,
-		// Additional performance options
-		defaultOptions: {
-			watchQuery: {
-				fetchPolicy: 'cache-first',
-				nextFetchPolicy: 'cache-first',
-			},
-		},
-	});
+    // Create optimized Apollo Client
+    return new ApolloClient({
+        // Configure cache
+        cache: new InMemoryCache({
+            typePolicies: {
+                Query: {
+                    fields: {
+                        // Add field policies here for cache behavior
+                    },
+                },
+            },
+        }),
+        // Configure network layer based on environment
+        link:
+            typeof window === 'undefined'
+                ? ApolloLink.from([
+                      // Enable streaming responses in SSR
+                      new SSRMultipartLink({
+                          stripDefer: true,
+                      }),
+                      httpLink,
+                  ])
+                : httpLink,
+        // Additional performance options
+        defaultOptions: {
+            watchQuery: {
+                fetchPolicy: 'cache-first',
+                nextFetchPolicy: 'cache-first',
+            },
+        },
+    });
 }
 
 export const ApolloWrapper = ({ children }) => {
-	const client = makeClient();
-	return <ApolloProvider client={client}>{children}</ApolloProvider>;
+    const client = makeClient();
+    return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };

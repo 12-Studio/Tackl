20acaa5d667b04a673bcfe02423cb318224da2c6
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-07-30 12:58:53 +0100

    ✨ Added semantic bases for all fonts
    Type: feature
    Build: ⏩ Skipped

--- src/theme/tackl/type.ts
+++ src/theme/tackl/type.ts
@@ -59,7 +59,7 @@ export const h0Styles: RuleSet<object> = css`
 `;
 
 
-export const h1Styles: RuleSet<object> = css`
+export const headingXXL: RuleSet<object> = css`
     ${baseHeadingStyles}
     font-size: 5.8rem;
     line-height: 1.2;
@@ -69,35 +69,35 @@ export const h1Styles: RuleSet<object> = css`
     `}
 `;
 
-export const h2Styles: RuleSet<object> = css`
+export const headingXL: RuleSet<object> = css`
     ${baseHeadingStyles}
     font-size: 4.8rem;
     line-height: 1.2;
     ${bp.xlarge`font-size: 9rem;`}
 `;
 
-export const h3Styles: RuleSet<object> = css`
+export const headingL: RuleSet<object> = css`
     ${baseHeadingStyles}
     font-size: 4rem;
     line-height: 1.32;
     ${bp.xlarge`font-size: 6rem;`}
 `;
 
-export const h4Styles: RuleSet<object> = css`
+export const headingM: RuleSet<object> = css`
     ${baseHeadingStyles}
     font-size: 3rem;
     line-height: 1.32;
     ${bp.large`font-size: 4.8rem;`}
 `;
 
-export const h5Styles: RuleSet<object> = css`
+export const headingSM: RuleSet<object> = css`
     ${baseHeadingStyles}
     font-size: 2.4rem;
     line-height: 1.32;
     ${bp.large`font-size: 4rem;`}
 `;
 
-export const h6Styles: RuleSet<object> = css`
+export const headingS: RuleSet<object> = css`
     ${baseHeadingStyles}
     font-size: 1.2rem;
     line-height: 1.32;
@@ -107,26 +107,9 @@ export const h6Styles: RuleSet<object> = css`
 `;
 
 // Body styles with memoized shared styles
-export const pStyles: RuleSet<object> = css`
+export const bodyM: RuleSet<object> = css`
     ${baseBodyStyles}
     font-size: 1.6rem;
     line-height: 1.44;
     ${bp.xlarge`font-size: 2.2rem;`}
-`;
-
-export const spanStyles: RuleSet<object> = css`
-    ${baseBodyStyles}
-    font-size: 1.4rem;
-    line-height: 1.32;
-    ${bp.xlarge`font-size: 1.8rem;`}
-`;
-
-export const emStyles: RuleSet<object> = css`
-    ${baseBodyStyles}
-    display: inline-block;
-    font-style: normal;
-    font-weight: ${medium};
-    font-size: 1rem;
-    line-height: 1.32;
-    ${bp.xlarge`font-size: 1.2rem;`}
 `;

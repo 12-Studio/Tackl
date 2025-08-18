9fba990eaad321065f740895a125591d3c856e74
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-08-18 18:25:07 +0100

    🐛 Fixed Grid Exposer
    Type: bug
    Build: ⏩ Skipped
    
    Notes: For some reason, legacy padding was being added to the new updated grid

--- src/components/GridExposer/styles.js
+++ src/components/GridExposer/styles.js
@@ -6,41 +6,36 @@ import { Main, bp, Div, Aside } from '@tackl';
 // Exports
 // ------
 export const Col = styled(Div)(
-	(props) => css`
-		height: 100%;
+    props => css`
+        height: 100%;
 
-		span {
-			display: block;
-			opacity: ${props.$altColor ? 0.5 : 0.2};
-			background-color: ${props.$altColor
-				? props.theme.colors.feedback.negative
-				: props.theme.colors.global.white.solid};
+        span {
+            display: block;
+            opacity: ${props.$altColor ? 0.5 : 0.2};
+            background-color: ${props.$altColor
+                ? props.theme.colors.feedback.negative
+                : props.theme.colors.global.white.solid};
 
-			width: 100%;
-			height: 100%;
-			transition: all 0.25s linear;
-		}
-	`
+            width: 100%;
+            height: 100%;
+            transition: all 0.25s linear;
+        }
+    `
 );
 
 export const Jacket = styled(Aside)(
-	(props) => css`
-		position: fixed;
-		top: 0;
-		left: 0;
-		z-index: 9999;
-		width: 100%;
-		height: ${props.$showGrid ? `100%` : `0%`};
-		pointer-events: none;
-		transition: all 1s ${props.theme.easing.bezzy};
-		padding: 0 1.2rem;
+    props => css`
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 9999;
+        width: 100%;
+        height: ${props.$showGrid ? `100%` : `0%`};
+        pointer-events: none;
+        transition: all 1s ${props.theme.easing.bezzy};
 
-		${bp.large`
-			padding: 0;
-		`}
-
-		> .grid {
-			height: 100%;
-		}
-	`
+        > .grid {
+            height: 100%;
+        }
+    `
 );

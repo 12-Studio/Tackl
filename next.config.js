b4a6398abab81bd2327cede9650f3897ecbe2135
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-09-08 00:04:52 +0100

    🐛 Fixed depricated image domains
    Type: bug
    Build: ⏩ Skipped

--- next.config.js
+++ next.config.js
@@ -6,7 +6,16 @@ const nextConfig = {
         minimumCacheTTL: 60,
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        domains: ['picsum.photos', 'placehold.it'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.it',
+            },
+        ],
     },
     transpilePackages: ['next-image-export-optimizer'],
 

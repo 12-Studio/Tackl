735f886e800d8ed429ee60e6ff94e2fe15d080c8
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-10-06 12:39:20 +0100

    ✨ Upgraded grid for dashed checking
    Type: feature
    Build: ⏩ Skipped

--- src/components/GridExposer/styles.ts
+++ src/components/GridExposer/styles.ts
@@ -1,38 +1,41 @@
 // Imports
 // ------
+import { Aside, Div, bp, getEase, getFeedback } from '@/theme/tackl';
 import styled, { css } from 'styled-components';
-import { bp, Div, Aside, getEase, getGlobal, getFeedback } from '@/theme/tackl';
-
 
 // Interfaces
 // ------
 interface ColProps {
-    $altColor?: boolean;
-    $isMobile?: boolean;
-    $isTablet?: boolean;
-    $isDesktop?: boolean;
+	$altColor?: boolean;
+	$isMobile?: boolean;
+	$isTablet?: boolean;
+	$isDesktop?: boolean;
 }
 
 interface JacketProps {
-    $showGrid?: boolean;
-    $altColor?: boolean;
+	$showGrid?: boolean;
+	$altColor?: boolean;
 }
 
 // Exports
 // ------
 export const Col = styled(Div)<ColProps>(
-    props => css`
-        height: 100%;
-        display: none;
+	props => css`
+		height: 100%;
+		display: none;
 
-        ${props.$isMobile && css`
-            display: block;
-        `}
+		${props.$isMobile &&
+		css`
+			display: block;
+		`}
 
-        ${bp.m`
-            ${props.$isTablet && css`
-                display: block;
-            `}
+		${bp.m`
+            ${
+				props.$isTablet &&
+				css`
+					display: block;
+				`
+			}
         `}
 
         ${bp.l`
@@ -40,32 +43,45 @@ export const Col = styled(Div)<ColProps>(
         `}
 
         span {
-            display: block;
-            opacity: ${props.$altColor ? 0.5 : 0.2};
-            background-color: ${props.$altColor
-                ? getFeedback('negative')
-                : getGlobal('white')};
+			--max: 100%;
+			display: block;
 
-            width: 100%;
-            height: 100%;
-            transition: all 0.25s linear;
-        }
-    `
+			border-inline-style: dashed;
+			border-inline-width: ${!props.$altColor ? 0 : 1}px;
+			border-inline-color: ${getFeedback('negative')};
+
+			width: var(--max);
+			height: var(--max);
+			transition: all 0.25s linear;
+
+			&:after {
+				content: '';
+				opacity: ${!props.$altColor ? 0.5 : 0.2};
+				display: block;
+				width: var(--max);
+				height: var(--max);
+				transition: all 0.25s linear;
+				background-color: ${!props.$altColor
+					? getFeedback('negative')
+					: 'transparent'};
+			}
+		}
+	`
 );
 
 export const Jacket = styled(Aside)<JacketProps>(
-    props => css`
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 9999;
-        width: 100%;
-        height: ${props.$showGrid ? `100%` : `0%`};
-        pointer-events: none;
-        transition: all 1s ${getEase('bezzy')};
+	props => css`
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 9999;
+		width: 100%;
+		height: ${props.$showGrid ? `100%` : `0%`};
+		pointer-events: none;
+		transition: all 1s ${getEase('bezzy')};
 
-        > .grid {
-            height: 100%;
-        }
-    `
+		> .grid {
+			height: 100%;
+		}
+	`
 );

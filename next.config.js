a1058b50d912262c2f6b1766a048579de7910f76
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-11-02 02:40:46 +0000

    ♻️ Removed depricated next16 config and updated readme to newest version of Tackl
    Type: refactor
    Build: ⏩ Skipped

--- next.config.js
+++ next.config.js
@@ -63,21 +63,6 @@ const nextConfig = {
 			minify: true,
 		},
 	},
-
-	// Headers configuration
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'Permissions-Policy',
-						value: 'camera=(), microphone=(), geolocation=()',
-					},
-				],
-			},
-		];
-	},
 };
 
 module.exports = nextConfig;
